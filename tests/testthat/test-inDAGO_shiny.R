0dfd12a6ff8d1c0289114551147f1b7a7d32f136
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-06-17 09:47:17 -0400

    remove installation

--- tests/testthat/test-inDAGO_shiny.R
+++ tests/testthat/test-inDAGO_shiny.R
@@ -1,24 +1,3 @@
-# Install Bioconductor dependencies if you don't have them yet
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-bioc_pac <- c(
-  "XVector",
-  "ShortRead",
-  "S4Vectors",
-  "rtracklayer",
-  "Rsubread",
-  "Rsamtools",
-  "limma",
-  "HTSFilter",
-  "edgeR",
-  "Biostrings",
-  "BiocGenerics"
-)
-for (pac in bioc_pac) {
-  if (!requireNamespace(pac, quietly = TRUE))
-    BiocManager::install(pac)
-}
-
 library("inDAGO")
 
 
